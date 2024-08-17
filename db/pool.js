5b10411b04adf9ede993d52b15810d09a2166fff
Sarfaroj Ansari <sarfarosha66@gmail.com>
2024-08-18 00:29:10 +0530

    fix bug connection string

--- db/pool.js
+++ db/pool.js
@@ -1,8 +1,5 @@
 const { Pool } = require("pg");
 
 module.exports = new Pool({
-  user: process.env.user,
-  host: process.env.host,
-  database: process.env.database,
-  password: process.env.password,
+  connectionString: process.env.CONNECTION_STRING,
 });
